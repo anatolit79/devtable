07c8588ca42e1506caf1cd94dd12030a40deea9a
anatolit79 <126828766+anatolit79@users.noreply.github.com>
2023-03-03 08:32:18 +1000

    Create index.php
    I did index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+
+<body class='b'>
+<style type='text/css'>
+.b {
+background:cyan;
+background-image: url(bg.bmp);
+}
+.d1 {
+color:red;
+font-style:bold;
+background:yellow;
+}
+.d2 {
+color:green;
+font-style:italic;
+/* display:none; */
+}
+a:link {
+color: red; /* Цвет ссылок */
+}
+а:hover {
+color: cyan; /* Цвет наводки */
+}  
+a:visited {
+color: blue; /* Цвет посещенных ссылок */
+}
+</style>
+
+<ul class='d1'> Модели машин
+<li> Nissan </li>
+<li> <a href='https://honda.ru'> Honda </a> 
+<ul class='d2'>
+<li> Бензин </li>
+<li> Дизель </li>
+<li> Электричество </li>
+</ul>
+</li>
+
+<li> Toyota </li>
+<li> Lada </li>
+</ul>
+
+</body>
+
+</html>
